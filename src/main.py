92637e2006cf6a58d64f29711d36f9838b7c5285
LostUnion <nsk.19980513@gmail.com>
2024-11-29 19:59:37 +0300

    The function of displaying all tasks or only by category has been added.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from typing import Optional, List
 import json
-import argparse
 
 class Task:
     """Описание класса Task"""
@@ -15,34 +14,52 @@ class Task:
         self.status = status
 
 class TaskManager:
-    """Описание класса TaskManager"""
 
-    pretty_printed_JSON = True
+    pretty_printed_JSON = False
+
+    """Описание класса TaskManager"""
 
     def __init__(self, path: str ="data.json", ):
         """Инициализируем класс"""
         self.path = path
 
-    def get_all_tasks(self):
-        """Получение всех текущих задач из файла data.json."""
-    
+    def get_task(self, value: str = ""):
+        """Получение всех текущих задач из файла data.json или получение по значению"""
+
         with open(self.path, "r", encoding="utf-8") as file:  # Открытие файла data.json для чтения
             data = json.load(file)  # Используем json.load для загрузки JSON из файла
 
-            if self.pretty_printed_JSON:  # Вывод в формате pretty-printed JSON, если pretty_printed_JSON = True
-                print(json.dumps(data, indent=4, ensure_ascii=False))
-            else:  # Вывод в пользовательском формате, если pretty_printed_JSON = False
-                for index, task in enumerate(data):
-                    formatting_data = (
-                        f"ID задания: {task['id']}\n"
-                        f"Название: {task['title']}\n"
-                        f"Описание: {task['description']}\n"
-                        f"Категория: {task['category']}\n"
-                        f"Срок выполнения: {task['due_date']}\n"
-                        f"Приоритет: {task['priority']}\n"
-                        f"Статус задачи: {task['status']}\n"
-                    )
-                    print(formatting_data)
+        # Функция форматирования задач в пользовательский вывод
+        def format_tasks(tasks):
+            formatting_data = ""
+            for task in tasks:
+                formatting_data += (
+                    f"ID задания: {task['id']}\n"
+                    f"Название: {task['title']}\n"
+                    f"Описание: {task['description']}\n"
+                    f"Категория: {task['category']}\n"
+                    f"Срок выполнения: {task['due_date']}\n"
+                    f"Приоритет: {task['priority']}\n"
+                    f"Статус задачи: {task['status']}\n\n"
+                )
+            return formatting_data.strip()
 
+        if value:
+            new_data = [task for task in data if task['category'] == value]
+            if new_data:
+                if self.pretty_printed_JSON:
+                    return json.dumps(new_data, indent=4, ensure_ascii=False)
+                else:
+                    return format_tasks(new_data)
+            else:
+                return f"Значение \"{value}\" не было найдено в файле."
+        else:
+            if self.pretty_printed_JSON:
+                return json.dumps(data, indent=4, ensure_ascii=False)
+            else:
+                return format_tasks(data)
+
+            
 task = TaskManager()
-task.get_all_tasks()
+t = task.get_task()
+print(t)
